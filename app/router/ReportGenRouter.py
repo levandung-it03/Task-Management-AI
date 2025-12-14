a7fee4fae5514283b7fbb4538ed0af2d72df329d
PLKhang <plkhang090403@gmail.com>
2025-12-14 18:07:55 +0700

    Update REPORT-GEN-ENDPOINT to v3

--- app/router/ReportGenRouter.py
+++ app/router/ReportGenRouter.py
@@ -7,7 +7,17 @@ from app.util.constants.Variables import Api
 router = APIRouter()
 PRIVATE_RPT_GEN_API_V1 = Api.API_PRIVATE_PREFIX + "/v1" + Api.API_REPORT_GEN
 
-@router.post(PRIVATE_RPT_GEN_API_V1 + "/generate")
-def generate_report_endpoint(req: ReportRequest) -> ReportResponse:
-    text = ReportGenSvc.generate_report(req)
+@router.post(PRIVATE_RPT_GEN_API_V1 + "/completed-report")
+def generate_completed_report_endpoint(req: ReportRequest) -> ReportResponse:
+    text = ReportGenSvc.generate_completed_report(req)
+    return ReportResponse(report=text)
+
+@router.post(PRIVATE_RPT_GEN_API_V1 + "/processing-report")
+def generate_processing_report_endpoint(req: ReportRequest) -> ReportResponse:
+    text = ReportGenSvc.generate_processing_report(req)
+    return ReportResponse(report=text)
+
+@router.post(PRIVATE_RPT_GEN_API_V1 + "/daily-report")
+def generate_daily_report_endpoint(req: ReportRequest) -> ReportResponse:
+    text = ReportGenSvc.generate_daily_report(req)
     return ReportResponse(report=text)
--- app/service/report_gen_svc.py
+++ app/service/report_gen_svc.py
@@ -16,16 +16,71 @@ class ReportGenSvc:
     REPORT_GEN_HOST = str(os.getenv(Env.NGROK_REPORT_GENT_HOST))
 
     @classmethod
-    def generate_report(cls, request: ReportRequest) -> str:
-        prompt = request.to_prompt()
+    def generate_completed_report(cls, request: ReportRequest) -> str:
+        prompt = request.to_completed_report_prompt()
         payload = {
             CstModelReq.model: CstModelCfg.MODEL_NAME,
             CstModelReq.messages: [
-                { CstModelReq.role: CstModelCfg.SYS_ROLE, CstModelReq.content: CstModelCfg.DEF_PROMPT_TOKEN },
+                { CstModelReq.role: CstModelCfg.SYS_ROLE, CstModelReq.content: CstModelCfg.DEF_COMPLETED_REPORT_TOKEN },
                 { CstModelReq.role: CstModelCfg.USR_ROLE, CstModelReq.content: prompt },
             ],
-            CstModelReq.temperature: CstModelCfg.DEF_TEMPERATURE,
-            CstModelReq.max_tokens: CstModelCfg.DEF_MAX_TOKENS
+            CstModelReq.temperature: CstModelCfg.DEF_COMPLETED_REPORT_TEMPERATURE,
+            CstModelReq.max_tokens: CstModelCfg.DEF_COMPLETED_REPORT_MAX_TOKENS,
+            CstModelReq.top_p: CstModelCfg.DEF_COMPLETED_REPORT_TOP_P
+        }
+        try:
+            response = requests.post(cls.REPORT_GEN_HOST, json=payload, timeout=20)
+            response.raise_for_status()
+
+            data = response.json()
+            text = data[CstModelRes.report]
+
+            logger.info(CstLog.res_success + CstModelCfg.MODEL_NAME)
+            return text.strip()
+
+        except Exception as e:
+            logger.error(CstLog.res_error + e)
+            raise RuntimeError(CstLog.res_def_throw)
+
+    @classmethod
+    def generate_processing_report(cls, request: ReportRequest) -> str:
+        prompt = request.to_processing_report_prompt()
+        payload = {
+            CstModelReq.model: CstModelCfg.MODEL_NAME,
+            CstModelReq.messages: [
+                { CstModelReq.role: CstModelCfg.SYS_ROLE, CstModelReq.content: CstModelCfg.DEF_PROCESSING_REPORT_TOKEN },
+                { CstModelReq.role: CstModelCfg.USR_ROLE, CstModelReq.content: prompt },
+            ],
+            CstModelReq.temperature: CstModelCfg.DEF_PROCESSING_REPORT_TEMPERATURE,
+            CstModelReq.max_tokens: CstModelCfg.DEF_PROCESSING_REPORT_MAX_TOKENS,
+            CstModelReq.top_p: CstModelCfg.DEF_PROCESSING_REPORT_TOP_P
+        }
+        try:
+            response = requests.post(cls.REPORT_GEN_HOST, json=payload, timeout=20)
+            response.raise_for_status()
+
+            data = response.json()
+            text = data[CstModelRes.report]
+
+            logger.info(CstLog.res_success + CstModelCfg.MODEL_NAME)
+            return text.strip()
+
+        except Exception as e:
+            logger.error(CstLog.res_error + e)
+            raise RuntimeError(CstLog.res_def_throw)
+
+    @classmethod
+    def generate_daily_report(cls, request: ReportRequest) -> str:
+        prompt = request.to_daily_report_prompt()
+        payload = {
+            CstModelReq.model: CstModelCfg.MODEL_NAME,
+            CstModelReq.messages: [
+                { CstModelReq.role: CstModelCfg.SYS_ROLE, CstModelReq.content: CstModelCfg.DEF_DAILY_REPORT_TOKEN },
+                { CstModelReq.role: CstModelCfg.USR_ROLE, CstModelReq.content: prompt },
+            ],
+            CstModelReq.temperature: CstModelCfg.DEF_DAILY_REPORT_TEMPERATURE,
+            CstModelReq.max_tokens: CstModelCfg.DEF_DAILY_REPORT_MAX_TOKENS,
+            CstModelReq.top_p: CstModelCfg.DEF_DAILY_REPORT_TOP_P
         }
         try:
             response = requests.post(cls.REPORT_GEN_HOST, json=payload, timeout=20)
